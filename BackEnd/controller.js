b3c6e27e964e4f5de0558b55b028bdd0e24e14c5
MoAboali <mohamed.aboali20@outlook.com>
2024-08-01 12:12:55 -0500

    CRUD of Game is working

--- BackEnd/controller.js
+++ BackEnd/controller.js
@@ -8,139 +8,75 @@ const ObjectId = require("mongodb").ObjectId;
 // Get Operations
 
 module.exports.getAllitems = function (req, res) {
-    itemModel.find().then((Data) => {
-        res.status(200).json(Data);
-    })
+    itemModel.find().then((Data) => { res.status(200).json(Data); })
         .catch((err) => {
             res.status(500).json({ error: err });
         });
 }
 
-
-const getOneitemCallbackified = callbackify(function (dataCollection, query) {
-    return dataCollection.findOne(query);
-});
 module.exports.getgetOneitembyid = function (req, res) {
-    getOneitemCallbackified(itemModel, { _id: new ObjectId(req.params.id) }, function (err, Data) {
-        if (err) { res.status(500).json({ error: err }); }
-        else { res.status(200).json(Data); }
-    });
+    itemModel.findOne({ _id: new ObjectId(req.params.id) }).then((Data) => { res.status(200).json(Data); })
+        .catch((err) => {
+            res.status(500).json({ error: err });
+        });
 
 }
 
 
 // Post Operations
 
-const createitembackified = callbackify((dataCollection, data) => {
-    return dataCollection.create(data);
-});
 exports.createitem = function (req, res) {
-
-    console.log(req.body);
-    const Newitem = {
-        Make: req.body.Make,
-        Model: req.body.Model,
-        Year: req.body.Year
-
-    };
-
-    try {
-        createitembackified(itemModel, Newitem, function (err, response) {
-            if (err) { res.status(500).json({ error: err }); }
-            else { res.status(201).json(response); }
+    const Newitem = { ...req.body };
+    itemModel.create(Newitem).then((response) => { res.status(201).json(response); })
+        .catch((err) => {
+            res.status(500).json({ error: err });
         });
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
 }
 
 
 //Delete Operations
 
-const deleteOneitemCallbackified = callbackify(function (dataCollection, query) {
-    return dataCollection.findByIdAndDelete(query);
-});
 module.exports.deletegetOneitembyid = function (req, res) {
-    deleteOneitemCallbackified(itemModel, { _id: new ObjectId(req.params.id) }, function (err, Data) {
-        if (err) { res.status(500).json({ error: err }); }
-        else { res.status(200).json({ message: "Item Deleted" }); }
-    });
+    itemModel.findByIdAndDelete({ _id: new ObjectId(req.params.id) })
+        .then((response) => { res.status(200).json({ message: "Item Deleted", ItemDeleted: response }); })
+        .catch((err) => {
+            res.status(500).json({ error: err });
+        });
+
 
 }
 
 
 // Update Operations
 
-
-const fullupdateitembackified = callbackify((dataCollection) => {
-    //return dataCollection.findByIdAndUpdate(query, data, { overwrite: false });, query, data, overwriteV
-    return dataCollection.save();
-});
 exports.fullupdateeitem = function (req, res) {
-
-    getOneitemCallbackified(itemModel, { _id: new ObjectId(req.params.id) }, function (err, Data) {
-        if (err) {
-            res.status(500).json({ error: err });
+    itemModel.findOneAndReplace(
+        { _id: new ObjectId(req.params.id) },
+        { ...req.body },
+        {
+            new: true, useFindAndModify: true, runValidators: true,
         }
-        else {
-
-
-            const Newitem = {
-                ...req.body,
-                publisher: Data.publisher,
-                reviews: Data.reviews
-            };
-
-            Object.keys(Data._doc).forEach(key => {
-                if (!Newitem.hasOwnProperty(key) && key != "_id") {
-                    delete Data._doc[key];
-                }
-                else if (key != "_id") {
-                    if (typeof Data._doc[key] === 'number') {
-                        Data._doc[key] = Number(Newitem[key]);
-                    }
-                    else {
-                        Data._doc[key] = Newitem[key];
-                    }
-
-
-                }
-
-            });
-
-
-            try {
-
-                fullupdateitembackified(Data, function (err, response) {  //, { _id: new ObjectId(req.params.id) }, Newitem, true
-                    if (err) { res.status(500).json({ error: err }); }
-                    else { res.status(201).json(response); }
-                });
-            } catch (err) {
-                res.status(400).json({ message: err.message });
+    )
+        .then((RData) => {
+            if (!RData) {
+                return res.status(404).json({ error: 'Item not found' });
             }
-
-
-
-
-        }
-    });
-
+            res.status(200).json(RData);
+        })
+        .catch((err) => {
+            res.status(500).json({ error: err });
+        });
 
 }
 
-const partialupdateitembackified = callbackify((dataCollection, query, data, overwriteV) => {
-    return dataCollection.findByIdAndUpdate(query, data, { new: true, overwrite: overwriteV });
-
-});
 exports.partialupdateeitem = function (req, res) {
-    try {
-        partialupdateitembackified(itemModel, { _id: new ObjectId(req.params.id) }, { $set: req.body }, false, function (err, response) {
-            if (err) { res.status(500).json({ error: err }); }
-            else { res.status(201).json(response); }
-        });
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
+    itemModel.findByIdAndUpdate({ _id: new ObjectId(req.params.id) }, { $set: req.body }, { new: true, overwrite: false }).then((Data) => {
+        res.status(201).json(Data);
+    }).catch((err) => {
+        res.status(500).json({ error: err });
+    });
+
+
 }
 
 
